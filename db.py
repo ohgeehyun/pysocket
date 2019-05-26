0fc8d980e6c6e15fd6c65d029fcc7c24f41549b0
ohgeehyun <chlghksn94@naver.com>
2019-05-26 11:08:28 +0100

    debug

--- db.py
+++ db.py
@@ -4,17 +4,12 @@ import time
 import serial
 db = pymysql.connect(host="localhost",user="pi",passwd="1111",db="pot_db")
 cur = db.cursor()
-ser = serial.Serial("/dev/ttyUSB0",9600)
+ser = serial.Serial("/dev/ttyAMA0",115200,timeout=1)
 ser.readline()
-while True:
-    try:
-        data = str(ser.readline())
-        sql = "INSERT INTO plant values(%s,%s,%s,%s,%s,%s)"
-        cur.execute(sql,(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()),data.split('/')[0],data.split('/')[1],data.split('/')[2],data.split('/')[3],data.split('/')[4].strip()))
-        db.commit()
-
-    except KeyboardInterrupt:
-        ser.close()
-        db.close()
-        break
+data = str(ser.readline())
+sql = "INSERT INTO plant values(%s,%s,%s,%s,%s,%s)"
+cur.execute(sql,(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()),data.split('/')[0],data.split('/')[1],data.split('/')[2],data.split('/')[3],data.split('/')[4].strip()))
+db.commit()
+ser.close()
+db.close()
             
